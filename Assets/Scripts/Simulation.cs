24a6e12a3fe6bdea3030a74fe439afa90c586e90
W1nter-IsHere <w1nter.ish3re@gmail.com>
2023-02-11 09:40:09 -0500

    it works!!!!

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -1,48 +1,62 @@
-using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using Screen = UnityEngine.Device.Screen;
 
 public class Simulation : MonoBehaviour
 {
     [SerializeField] private ComputeShader simulationShader;
-    [SerializeField] private ComputeShader fadeoutShader;
-    
-    private RenderTexture _mainTexture;
-    private List<Slime> _slimes;
+    [SerializeField] private ComputeShader postProcessingShader;
+    [SerializeField, Min(1)] private int updateInterval;
 
+    private int _tick;
+    private RenderTexture _mainTexture;
+    private Slime[] _slimes;
+    
     private void Start()
     {
-        _mainTexture = new RenderTexture(256, 256, 24, GraphicsFormat.R16G16B16A16_SFloat);
+        _mainTexture = new RenderTexture(Screen.width, Screen.height, 24, GraphicsFormat.R16G16B16A16_SFloat);
         _mainTexture.enableRandomWrite = true;
         _mainTexture.Create();
 
-        _slimes = new List<Slime>();
-        _slimes.Add(new Slime {Angle = Mathf.Deg2Rad * 45, Position = new int2(128, 128), Speed = 10});
-        _slimes.Add(new Slime {Angle = Mathf.Deg2Rad * 25, Position = new int2(128, 128), Speed = 10});
+        _slimes = new Slime[36000];
+        // _slimes[0] = new Slime(new int2(128, 128), Mathf.Deg2Rad * 25);
+        // _slimes[1] = new Slime(new int2(128, 128), Mathf.Deg2Rad * 45);
+
+        var i = 0;
+        for (var a = 0f; a < 360; a += 0.01f)
+        {
+            _slimes[i] = new Slime(new int2(_mainTexture.width / 2, _mainTexture.height / 2), Mathf.Deg2Rad * a);
+            i++;
+        }
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        var buffer = new ComputeBuffer(_slimes.Count, sizeof(int) * 2 + sizeof(float) * 2);
-        buffer.SetData(_slimes);
-        
-        simulationShader.SetBuffer(0, "slimes", buffer);
-        simulationShader.SetTexture(0, "result", _mainTexture);
-        simulationShader.SetInt("width", _mainTexture.width);
-        simulationShader.SetInt("height", _mainTexture.height);
-        simulationShader.SetInt("sensory_range", _mainTexture.width / 10);
-        simulationShader.Dispatch(0, Mathf.CeilToInt(_slimes.Count / 16f), 1, 1);
+        var deltaTime = Time.deltaTime;
 
-        var newSlimes = new Slime[_slimes.Count];
-        buffer.GetData(newSlimes);
-        _slimes.Clear();
-        _slimes.AddRange(newSlimes);
+        if (_tick % updateInterval == 0)
+        {
+            var slimesBuffer = new ComputeBuffer(_slimes.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Slime)));
+            slimesBuffer.SetData(_slimes);
+            simulationShader.SetBuffer(0, "slimes", slimesBuffer);
+            simulationShader.SetTexture(0, "result", _mainTexture);
+            simulationShader.SetInt("width", _mainTexture.width);
+            simulationShader.SetInt("height", _mainTexture.height);
+            simulationShader.SetInt("sensory_range", _mainTexture.width / 10);
+            simulationShader.SetFloat("delta_time", deltaTime);
+            simulationShader.Dispatch(0, Mathf.CeilToInt(_slimes.Length / 16f), 1, 1);
+            slimesBuffer.GetData(_slimes);
         
-        fadeoutShader.SetTexture(0, "result", _mainTexture);
-        fadeoutShader.Dispatch(0, _mainTexture.width / 8, _mainTexture.height / 8, 1);
-
-        buffer.Dispose();
+            postProcessingShader.SetTexture(0, "result", _mainTexture);
+            postProcessingShader.SetFloat("delta_time", deltaTime);
+            postProcessingShader.SetInt("width", _mainTexture.width);
+            postProcessingShader.SetInt("height", _mainTexture.height);
+            postProcessingShader.Dispatch(0, Mathf.CeilToInt(_mainTexture.width / 8f), Mathf.CeilToInt(_mainTexture.height / 8f), 1);
+            slimesBuffer.Dispose();
+        }
+        
+        _tick++;
         Graphics.Blit(_mainTexture, dest);
     }
 }
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -5,4 +5,13 @@ public struct Slime
     public int2 Position;
     public float Angle;
     public float Speed;
+    public uint Valid;
+
+    public Slime(int2 position, float angle, float speed = 10)
+    {
+        Position = position;
+        Angle = angle;
+        Speed = speed;
+        Valid = 1;
+    }
 }
